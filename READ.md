5d78808a85fd4daa95c7c2698ff188759c7036ff
Catiflam <gilfrid1@gmail.com>
2023-07-28 15:37:40 +0200

    first commit

---
+++ READ.md
@@ -0,0 +1,11 @@
+# 1 ESERCIZIO
+
+- Chiedi all'utente un primo numero, poi un secondo e genera un numero casuale compreso tra i due.
+
+# 2 ESERCIZIO
+
+- Dare la possibilità di inserire due parole. Verificare che le due parole abbiano la stessa lunghezza. Se hanno la stessa lunghezza, stamparle entrambe altrimenti stampare la più lunga delle due.
+
+# 3 ESERCIZIO
+
+-Crea un array vuoto e chiedi all'utente un numero da inserire nell'array. Continua a chiedere i numeri all'utente e a inserirli nell'array fino a quando la somma degli elementi è minore di 50.
---
+++ js/main.js
@@ -0,0 +1,21 @@
+// const firstNumber = parseInt(prompt("inserisci il primo numero"));
+// const secondNumber = parseInt(prompt("inserisci il secondo numero"));
+
+// const calcNumber = Math.floor(Math.random() * (firstNumber - secondNumber + 1)) + secondNumber;
+// console.log(calcNumber);
+
+// const firstWord = prompt("inserisci una parola");
+// const secondWord = prompt("inserisci la seconda parola");
+
+// if (firstWord.length == secondWord.length) {
+// 	console.log(firstWord + "" + secondWord);
+// } else if (secondWord.length < firstWord.length) {
+// 	console.log(secondWord);
+// } else {
+// 	console.log(firstWord);
+// }
+
+const empty = [];
+
+let =0
+while ( )
